5c2e8d3b85afab53e7d5188dee597e72d4e174f7
MCVM <129513410+MinecraftVM@users.noreply.github.com>
2025-05-25 13:15:17 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -182,8 +182,7 @@ TW_FRAMERATE := 60
 
 # PBRP specific build flags
 PB_OFFICIAL := true
-PB_MAINTAINER := Minecraft-VM
-PB_MAINTAINER_INFO := @MinecraftVM (GitHub: MinecraftVM)
+PB_MAINTAINER := MinecraftVM
 PB_DISABLE_DEFAULT_DM_VERITY := true
 PB_DISABLE_DEFAULT_TREBLE_COMP := true
 PB_DEFAULT_THEME := pitchblack
